86ca3dfb5d6ed6810487a5bdda58e51e0bf18914
Micodexx <miracle.abaye@fairexxitsolutions.com>
2022-03-31 02:37:13 +0100

    converted currentPaginationData to a function at BlogList.jsx, passing values as paramters at the usePagination.js. added end of the line: auto to eslint configuration

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,13 +1,6 @@
 export const DOTS = "...";
 
-function usePagination() {
-  /*
-    Rewrite the logic here to map out the pagination to be displayed
-
-    !!!!!! ATTENTION !!!!!!
-    Please replace this comment here with a description of this hook.
-    
-  */
+function usePagination(/* { currentPage, totalCount, pageSize } */) {
   return [1, 2, 3, DOTS, 5];
 }
 
